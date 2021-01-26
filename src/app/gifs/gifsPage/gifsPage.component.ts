5e4f9fd61b119517cad3b85a6069bb5141f376a4
Javi <javi.cb@gmail.com>
2021-01-26 19:14:30 +0100

    gifsmodule y sus componentes

---
+++ src/app/gifs/gifsPage/gifsPage.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-gifsPage',
+  templateUrl: './gifsPage.component.html',
+  styleUrls: ['./gifsPage.component.css']
+})
+export class GifsPageComponent {
+
+  constructor() { }
+}
