e2c6827803744f9cdf592e9875bda8897929f0fb
Javier Cruz <javier.cruz.bonoso@avanade.com>
2023-05-26 01:08:29 +0200

    localstorage data persistence

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,9 @@ export class GifsService {
   private _tagsHistory: string[] = [];
   private apiKey = environment.giphy_api;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
@@ -22,8 +24,9 @@ export class GifsService {
    * organizeHistory
    * * Organize the search history of tags
    * @param tag param to add to the history
+   * @returns void
    */
-  private organizeHistory(tag: string) {
+  private organizeHistory(tag: string) : void {
     tag = tag.toLowerCase();
 
     if (this._tagsHistory.includes(tag) ) {
@@ -32,12 +35,14 @@ export class GifsService {
 
     this._tagsHistory.unshift( tag );
     this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
   }
 
   /**
    * searchTag
    * * Search for GIFs based on the provided tag
    * @param tag param to add to the history
+   * @returns void
    */
   searchTag( tag: string ) : void {
     if ( tag.trim().length === 0 ) {
@@ -57,4 +62,28 @@ export class GifsService {
       }
     );
   }
+
+  /**
+   * saveLocalStorage
+   * * Save the search history of tags in the local storage
+   * @returns void
+   */
+  private saveLocalStorage(): void {
+    localStorage.setItem('tagsHistory', JSON.stringify(this._tagsHistory));
+  }
+
+  /**
+   * loadLocalStorage
+   * * Load the search history of tags from the local storage
+   * * If there is no history, it will not load anything
+   * * If there is history, it will load the first tag
+   * @returns void
+   */
+  private loadLocalStorage(): void {
+    if (localStorage.getItem('tagsHistory')) {
+      this._tagsHistory = JSON.parse(localStorage.getItem('tagsHistory')!);
+      this.searchTag(this._tagsHistory[0]);
+    }
+  }
+
 }
