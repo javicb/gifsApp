3aadeddd5ccd2897faade4a571b249c7cab27847
Javier Cruz <javier.cruz.bonoso@avanade.com>
2023-05-26 01:59:49 +0200

    lazy image

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
@@ -17,7 +19,8 @@ import { CardComponent } from './components/card/card.component';
     SearchBoxComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent,
---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+  styleUrls: ['./lazy-image.component.css']
+})
+export class LazyImageComponent implements OnInit {
+
+  @Input() url!: string;
+  @Input() alt!: string;
+  public _loaded = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if (!this.url) {
+      throw new Error('Attribute url is required');
+    }
+  }
+
+  onLoad(): void {
+    this._loaded = true;
+  }
+
+}
